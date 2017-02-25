7f5f8b501228e12cc3071fd5623e413de4bc691d
thecarterb <cbawsome77@gmail.com>
2017-02-25 11:57:49 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,14 +4,12 @@ CURRENT_DIR=pwd
 echo -e "Installing shodan"
 pip install shodan > /dev/null
 echo -e "Done!"
-mkdir $CURRENT_DIR/tools
+mkdir tools
 cd tools
 echo -e "Installing additional tools"
-wget https://downloads.sourceforge.net/project/sslscan/sslscan/sslscan-1.8.2.tgz
-tar xzf sslscan-1.8.2.tgz -C sslscan > /dev/null
-git clone https://github.com/gkbrk/slowloris
+pip install sslyze > /dev/null
+git clone https://github.com/gkbrk/slowloris > /dev/null
 cd slowloris
-pip install -r requirements.txt
 python setup.py install
 cd ../..
 echo -n "What is your shodan API key?"
