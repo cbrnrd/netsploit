258b6eebce2cd41462f0a4ec6d25fb4d03f87eb8
thecarterb <cbawsome77@gmail.com>
2017-02-25 11:46:49 -0500

    add slowloris attack

--- setup.sh
+++ setup.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+CURRENT_DIR=pwd
 echo -e "Installing shodan"
 pip install shodan > /dev/null
 echo -e "Done!"
-#echo -e "Installing sslize"
-#git clone https://github.com/iSECPartners/sslyze > /dev/null
+mkdir $CURRENT_DIR/tools
+cd tools
+echo -e "Installing additional tools"
+wget https://downloads.sourceforge.net/project/sslscan/sslscan/sslscan-1.8.2.tgz
+tar xzf sslscan-1.8.2.tgz -C sslscan > /dev/null
+git clone https://github.com/gkbrk/slowloris
+cd slowloris
+pip install -r requirements.txt
+python setup.py install
+cd ../..
 echo -n "What is your shodan API key?"
 read key
 echo $key > .api_key
