5190849eca53121764cca84fcfb477bd67406f58
thecarterb <cbawsome77@gmail.com>
2017-02-25 16:21:36 -0500

    add internet command

--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# As of now, this script only works on Debian and OSX, but that's
+# just because of the package managers installing zmap
+
+os = uname
 echo -e "Installing shodan"
 pip install shodan > /dev/null
 echo -e "Done!"
 mkdir tools
 cd tools
+echo ""
 echo -e "Installing additional tools"
 pip install sslyze > /dev/null
 git clone https://github.com/gkbrk/slowloris > /dev/null
@@ -18,6 +23,15 @@ bash install.sh > /dev/null
 cd ..
 git clone https://github.com/thecarterb/hb-test-copy heartbleed > /dev/null
 cd ..
+
+if [$os == "Darwin"]; then
+  echo -e "Installing zmap"
+  brew install zmap > /dev/null
+else
+  echo -e "Installing zmap"
+  apt-get install -y zmap > /dev/null
+fi
+
 echo -n "What is your shodan API key? "
 read key
 echo $key > .api_key
