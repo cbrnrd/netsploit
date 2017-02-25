21080229c3212391f98f9882e6515d145ee66951
thecarterb <cbawsome77@gmail.com>
2017-02-25 10:55:56 -0500

    change banner

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo -e "Installing shodan"
 pip install shodan > /dev/null
 echo -e "Done!"
 #echo -e "Installing sslize"
-#git clone https://github.com/iSECPartners > /dev/null
+#git clone https://github.com/iSECPartners/sslyze > /dev/null
 echo -n "What is your shodan API key?"
 read key
 echo $key > .api_key
