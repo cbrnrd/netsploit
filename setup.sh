30c1870dac5a67d48d69561f23bbf6d9c5563e17
thecarterb <cbawsome77@gmail.com>
2017-02-25 15:46:19 +0000

    add setup.sh and read key from file

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo -e "Installing shodan"
+pip install shodan > /dev/null
+echo -e "Done!"
+#echo -e "Installing sslize"
+#git clone https://github.com/iSECPartners > /dev/null
+echo -n "What is your shodan API key?"
+read key
+echo $key > .api_key
