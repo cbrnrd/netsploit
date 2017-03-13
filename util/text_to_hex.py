f23efc8312f55dd8b53da81550e50cb156822d7d
Carter <cbawsome77@gmail.com>
2017-03-13 08:21:39 -0400

    Create text_to_hex.py

---
+++ util/text_to_hex.py
@@ -0,0 +1,13 @@
+def toHex(s):
+    lst = []
+    for ch in s:
+        hv = hex(ord(ch)).replace('0x', '')
+        if len(hv) == 1:
+            hv = '0'+hv
+        lst.append(hv)
+    
+    return reduce(lambda x,y:x+y, lst)
+    
+if __name__ == "__main__":
+    inp = raw_input("String to convert: ")
+    print toHex(inp)
